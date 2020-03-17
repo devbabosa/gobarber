4672f836a2276bb20285f87b517f85910a04422c
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-03-17 20:52:18 -0300

    loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,15 @@
-import { Router } from "express";
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => res.json({ message: "hello world" }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Humberto',
+    email: 'humberto.obarbosa@gmail.com',
+    password_hash: 'asdsad',
+  });
+  res.json(user);
+});
 
 export default routes;
