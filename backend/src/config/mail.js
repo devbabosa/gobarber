b88c90a392b4a6a3da00fe40a78bf2f603eeabc1
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-03-19 19:28:49 -0300

    configurando nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '188ebd763c8bc5',
+    pass: 'c7ffb58d064f82',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
